c958efdd8e74640a7da0638e8083bb26a1bc1179
Aman0246jploft <amankashyap0246jploft@gmail.com>
2026-03-12 16:45:26 +0530

    Save

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, Suspense  } from "react";
 import { Col, Container, Form, Nav, Row, Tab } from "react-bootstrap";
 import PhoneInput, { parsePhoneNumber } from "react-phone-number-input";
 import "react-phone-number-input/style.css";
@@ -8,7 +8,7 @@ import authApi from "@/api/authApi";
 import { useRouter, useSearchParams } from "next/navigation";
 import toast from "react-hot-toast";
 
-export default function Page() {
+ function RegisterForm() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [selectedTab, setSelectedTab] = useState("Customer");
@@ -583,5 +583,14 @@ export default function Page() {
         </Row>
       </Container>
     </div>
+  );
+}
+
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RegisterForm />
+    </Suspense>
   );
 }
